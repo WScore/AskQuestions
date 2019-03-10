4613b407ce9abb32e8ae0726ed03cf092ca8c33e
asaokamei <asao.kamei@gmail.com>
2019-03-10 10:59:49 +0900

    touched up html/css for improved visuals...

--- demo/index.php
+++ demo/index.php
@@ -11,19 +11,16 @@ $forms->setLabelClass('form-label');
 <html lang="en">
 <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <title>Ask Forms Demo - HTML Forms</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <style>
-        label.form-label {
-            font-weight: bold;
-            color: #496282;
-        }
-    </style>
+    <link rel="stylesheet" href="ask.css">
 </head>
 <body>
 <div class="container">
-    <h1>Ask Forms<span class="small"> /HTML Forms</span></h1>
+    <h1>Ask Forms<span class="small"> /Forms</span></h1>
+    <hr>
     <form action="">
         <div class="form-group">
             <?php $element = $forms->getElement('name'); ?>
